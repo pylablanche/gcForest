dd814294cebde470b412c1eb68263e01934b1ec1
pylablanche <plablanche@aims.ac.za>
2017-03-10 17:29:15 +0200

    Code style inspection done

--- GCForest.py
+++ GCForest.py
@@ -1,23 +1,23 @@
 import itertools
 import numpy as np
-# import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score
 
 
+# noinspection PyUnboundLocalVariable
 class gcForest(object):
-
     def __init__(self, n_tree=51, n_cascadeRF=1, target_accuracy=None):
 
-        setattr(self, 'n_layer', 1)
+        setattr(self, 'n_layer', 0)
         setattr(self, 'n_cascadeRF', int(n_cascadeRF))
         setattr(self, 'n_tree', int(n_tree))
         setattr(self, 'target_accuracy', target_accuracy)
 
+    # noinspection PyUnusedLocal
     def load_data(self, X, y, shape_1X, safe=False):
 
-        for attr in ['X','y','shape_1X']:
+        for attr in ['X', 'y', 'shape_1X']:
             if safe and not hasattr(self, attr):
                 setattr(self, attr, eval(attr))
             elif not safe:
@@ -58,24 +58,24 @@ class gcForest(object):
         pred_prob_crf = crf.predict_proba(sliced_X)
         pred_prob = np.c_[pred_prob_prf, pred_prob_crf]
 
-        return pred_prob.reshape([np.shape(self.X)[0],-1])
+        return pred_prob.reshape([np.shape(self.X)[0], -1])
 
     def _window_slicing_img(self, window):
 
         dims = getattr(self, 'shape_1X')
         if any(s < window for s in dims):
-             raise ValueError('window must be smaller than both dimensions for an image')
+            raise ValueError('window must be smaller than both dimensions for an image')
 
         sliced_imgs = []
         sliced_target = []
-        refs = np.arange(0, (dims[0]-window)*dims[1], dims[1])
+        refs = np.arange(0, (dims[0] - window) * dims[1], dims[1])
 
-        iterx = list(range(dims[0]-window+1))
-        itery = list(range(dims[1]-window+1))
+        iterx = list(range(dims[0] - window + 1))
+        itery = list(range(dims[1] - window + 1))
 
         for img, ix, iy in itertools.product(enumerate(self.X), iterx, itery):
             rind = refs + ix + dims[0] * iy
-            sliced_imgs.append(np.ravel([img[1][i:i+window] for i in rind]))
+            sliced_imgs.append(np.ravel([img[1][i:i + window] for i in rind]))
             sliced_target.append(self.y[img[0]])
 
         return np.asarray(sliced_imgs), np.asarray(sliced_target)
@@ -84,31 +84,35 @@ class gcForest(object):
 
         dims = getattr(self, 'shape_1X')
         if dims[0] < window:
-             raise ValueError('window must be smaller than the sequence dimension')
+            raise ValueError('window must be smaller than the sequence dimension')
 
         sliced_sqce = []
         sliced_target = []
 
         for sqce in enumerate(self.X):
-            slice_sqce = [sqce[1][i:i+window] for i in np.arange(dims[0]-window+1)]
+            slice_sqce = [sqce[1][i:i + window] for i in np.arange(dims[0] - window + 1)]
             sliced_sqce.append(slice_sqce)
-            sliced_target.append(np.repeat(self.y[sqce[0]], dims[0]-window+1))
+            sliced_target.append(np.repeat(self.y[sqce[0]], dims[0] - window + 1))
 
-        return np.reshape(sliced_sqce, [-1,window]), np.ravel(sliced_target)
+        return np.reshape(sliced_sqce, [-1, window]), np.ravel(sliced_target)
 
     def cascade_forest(self, max_layers=5):
 
         accuracy = -np.inf
-        feat_arr = getattr(self, 'mgs_features')
+        tgt_acc = getattr(self, 'target_accuracy')
+        mgs_feat = getattr(self, 'mgs_features')
+        feat_arr = mgs_feat
 
-        while accuracy < self.target_accuracy or self.n_layer <= max_layers:
+        while accuracy < tgt_acc or self.n_layer <= max_layers:
             self.n_layer += 1
             prf_crf_pred = self._cascade_layer(feat_arr)
             layer_pred_prob = np.mean(prf_crf_pred, axis=0)
             layer_pred = np.argmax(layer_pred_prob, axis=1)
             accuracy = accuracy_score(y_true=self.y, y_pred=layer_pred)
-            if accuracy < self.target_accuracy :
-                feat_arr = np.concatenate([np.asarray(prf_crf_pred),feat_arr], axis=1)
+            if accuracy < tgt_acc:
+                swap_pred = np.swapaxes(prf_crf_pred, 0, 1)
+                add_feat = swap_pred.reshape([np.shape(self.X)[0], -1])
+                feat_arr = np.concatenate([add_feat, mgs_feat], axis=1)
 
         return layer_pred
 
